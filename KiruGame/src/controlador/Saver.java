894eba7ac29f9dd83180fb43003d1007b11d52d7
unknown <franco.tume@tektonlabs.com>
2015-10-06 01:40:51 -0500

    guardar y cargar desde un archivo dado

--- KiruGame/src/controlador/Saver.java
+++ KiruGame/src/controlador/Saver.java
@@ -1,5 +1,13 @@
 package controlador;
 import modelo.Jugador;
+import modelo.Objeto;
+
+import com.thoughtworks.xstream.*;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import controlador.Mapa;
 import controlador.GestorMapas;
 import vista.Juego;
@@ -10,17 +18,63 @@ public class Saver {
 	
 	public Jugador p1;
 	public Jugador p2;
-	public Mapa map;
-	public int  vida;
 	
+	public ArrayList<AccionEspecial> acciones= new ArrayList<AccionEspecial>();
+	public ArrayList<Objeto> objetos= new ArrayList<Objeto>();
+	public ArrayList<Enemigo> enemigos= new ArrayList<Enemigo>();
+	public int  vida;
+	public int token;
 	public Saver()
 	{
 		this.currentLevel = Juego.currentLevel;
 		this.cantNivel    = Juego.cantNivel;
 		this.p1           = Mapa.p1;
 		this.p2           = Mapa.p2;
-		this.map          = GestorMapas.map;
-		this.vida         = Mapa.p1.getVida();
+		this.acciones     = GestorMapas.map.getAcciones();
+		this.objetos      = GestorMapas.map.getObjetos();
+		this.enemigos     = GestorMapas.map.getEnemigos();
+		this.token        = 1;
+		this.vida         = GestorMapas.map.p1.getVida();
+	}
+	
+	public void guardar()
+	{
+		  try {XStream xs = new XStream();
+          // 1. Escribir el archivoFileWriter 
+          FileWriter fw = new FileWriter("save1.xml");
+          fw.write(xs.toXML(this));
+          fw.close();
+         
+		  } catch (IOException e) {
+         System.out.println(e.toString());
+      		}
+		  
+	}
+	
+	public void cargar(String data)
+	{
+		 try {XStream xs = new XStream();
+         
+		 FileReader fr = new FileReader(data);
+	        Saver newsaver = (Saver)xs.fromXML(fr);
+	        
+	        fr.close();
+	        Mapa.p1 = newsaver.p1;
+	        Mapa.p2 = newsaver.p2;
+	        Juego.currentLevel = newsaver.currentLevel;
+			
+			GestorMapas.map.setAcciones(newsaver.acciones);
+			GestorMapas.map.setObjetos(newsaver.objetos);
+			GestorMapas.map.setEnemigos(newsaver.enemigos);
+			
+			GestorMapas.map.p1.setVida(newsaver.vida);
+	        
+	        
+		  } catch (IOException e) {
+        System.out.println(e.toString());
+     		}
+		
+		
 	}
 
 }
